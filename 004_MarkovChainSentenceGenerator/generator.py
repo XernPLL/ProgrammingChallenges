7cc7c834b7c0dae2c28c363357a47b4691bbba40
Xern <xernpl@gmail.com>
2019-03-13 15:52:56 +0100

    Done

--- 004_MarkovChainSentenceGenerator/generator.py
+++ 004_MarkovChainSentenceGenerator/generator.py
@@ -1,59 +1,28 @@
 import random
 
-###########   TODO: MAKE IT BETTER
+#speeches.txt from https://github.com/ryanmcdermott/trump-speeches
 
-#copypasta.txt from https://github.com/louisabraham/copypasta-data
-#sampletext.txt from https://github.com/lachlanpage/Markov-Chain-Sentence-Generator/blob/master/sampletext.txt
-licznik = 0
 dict = {}
-with open("copypasta.txt","r") as file:
-    for i in file:
-        sentence = i.rstrip("\n")
-        sentence = sentence.split()
-        try:
-            for n, j in enumerate(sentence):
-                if j+" "+ sentence[n+1] in dict.keys():
-                    dict[j + " " + sentence[n + 1]].add(sentence[n + 2])
-
-                else:
-                    dict[j + " " + sentence[n + 1]] = set()
-                    dict[j + " " + sentence[n + 1]].add(sentence[n + 2])
-        except IndexError:
-            pass
-
-with open("sampletext.txt","r") as file:
-    for i in file:
-        sentence = i.rstrip("\n")
-        sentence = sentence.split()
-        try:
-            for n, j in enumerate(sentence):
-                if j+" "+ sentence[n+1] in dict.keys():
-                    dict[j + " " + sentence[n + 1]].add(sentence[n + 2])
-
-                else:
-                    dict[j + " " + sentence[n + 1]] = set()
-                    dict[j + " " + sentence[n + 1]].add(sentence[n + 2])
-        except IndexError:
-            pass
-
-creator = []
-key, value = random.choice(list(dict.items()))
-key = key.split()
-creator.append(key[0])
-creator.append(key[1])
-wybor = random.choice(list(value))
-creator.append(wybor)
-
-try:
-    while licznik < 10:
-        value = random.choice(list(dict[key[1]+" "+ wybor]))
-        creator.append(value)
-        key[1] = wybor
-        wybor = value
-        licznik = licznik +1
-
-
-except:
-    print(" ".join(creator))
-
-print(" ".join(creator))
+with open("speeches.txt","r",encoding="utf8") as file:
+    text = file.read().split()
+    for i in range(len(text)-1):
+        word1 = text[i]
+        word2 = text[i+1]
+        if word1 in dict.keys():
+            dict[word1].append(word2)
+        else:
+            dict[word1] = [word2]
+            
+n = int(input("How long sentence do u need?(Bigger than 0)"))
+while(n < 1):
+    n = int(input("How long sentence do u need?(Bigger than 0)"))
+first = random.choice(list(dict.keys()))
+while first.islower():
+    first = random.choice(list(dict.keys()))
+sentence = [first]
+i = 0
+while i<n-1:
+    next = random.choice(list(dict[sentence[i]]))
+    sentence.append(next)
+    i = i + 1
+print(" ".join(sentence))
