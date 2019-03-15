c1253d6e6a9a2eb03856134fd4364fa191b7c3d0
Xern <xernpl@gmail.com>
2019-03-15 17:08:44 +0100

    Almost done

--- 007_StockMarketSimulator/simulator.py
+++ 007_StockMarketSimulator/simulator.py
@@ -9,7 +9,6 @@ import math
 import datetime
 
 data = pd.read_csv("NFLX.csv")
-original = data
 data["DayChange"] = ((data["Adj Close"] - data["Open"])/data["Open"])*100.0
 data["HighLowChange"] = ((data["High"] - data["Low"])/data["Adj Close"])*100.0
 data = data[["Adj Close", "HighLowChange", "DayChange", "Volume"]]
@@ -36,30 +35,5 @@ confidence = classifier.score(X_test, y_test)
 
 predict_done = classifier.predict(X_actually)
 
-
-
-style.use("ggplot")
-
-data["Predykcja"] = np.nan
-czas= np.array(original["Date"])
-spec = czas[-1].split("-")
-last = datetime.datetime(int(spec[0]), int(spec[1]), int(spec[2])).timestamp()
-if datetime.datetime(int(spec[0]), int(spec[1]), int(spec[2])).weekday() == 4:
-    next = last + 86400*3
-else:
-    next = last + 86400
-for i in predict_done:
-    next_date = datetime.datetime.fromtimestamp(next)
-    if next_date.timetuple().tm_wday == 4:
-        next = last + 86400 * 3
-    else:
-        next += 86400
-    data.loc[next_date] = [np.nan for _ in range(len(data.columns)-1)]+[i]
-plt.plot(original["Date"], data["Adj Close"])
-data['Predykcja'].plot()
-plt.legend(loc=4)
-plt.xlabel('Date')
-plt.ylabel('Price')
-plt.show()
 
 print(predict_done, confidence, predict_days)
